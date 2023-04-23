b0fa9803f274f5750cc12a1e8ab02a0af04ec14c
Denys Zaitsev <denzai83@gmail.com>
2023-04-23 20:54:22 +0300

    Add hw10

---
+++ hw10/quotes_to_scrape/users/views.py
@@ -0,0 +1,43 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm, LoginForm
+
+
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='quotes:root')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='quotes:root')
